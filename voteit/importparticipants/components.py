ec84c7a3dabec0148028a17d72ee9972dc5a0a85
Fredrik Nilson <sassur@gmail.com>
2012-06-04 15:04:45 +0200

    adding menu entry for import participants

---
+++ voteit/importparticipants/components.py
@@ -0,0 +1,14 @@
+from pyramid.url import resource_url
+from betahaus.viewcomponent import view_action
+
+from voteit.core.security import MANAGE_GROUPS
+
+from voteit.importparticipants import VoteITImportParticipants as _
+
+
+@view_action('participants_menu', 'add_participants', title = _(u"Import participants"), link = "@@add_participants", permission = MANAGE_GROUPS)
+def generic_menu_link(context, request, va, **kw):
+    """ This is for simple menu items for the meeting root """
+    api = kw['api']
+    url = "%s%s" % (api.meeting_url, va.kwargs['link'])
+    return """<li><a href="%s">%s</a></li>""" % (url, api.translate(va.title))
